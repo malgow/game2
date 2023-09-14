3eba54dae1f6a026795cef6b5e907fbd7c229818
malgow <malachig0w@hotmail.com>
2023-09-14 14:12:17 +0100

    /

---
+++ playerMovement.cs
@@ -0,0 +1,20 @@
+using Godot;
+
+public partial class playerMovement : CharacterBody2D
+{
+	[Export]
+	public int Speed { get; set; } = 400;
+
+	public void GetInput()
+	{
+
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		Velocity = inputDirection * Speed;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		GetInput();
+		MoveAndSlide();
+	}
+}
